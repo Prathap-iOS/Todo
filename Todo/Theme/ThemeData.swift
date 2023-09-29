48a53a1bb8aa98cc4c35f7f0c05e3e00efa76e61
NowFloats <prathap.reddy@nowfloats.com>
2023-09-29 19:06:20 +0530

    Todo app completed with CoreData

---
+++ Todo/Theme/ThemeData.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeData.swift
+//  Todo
+//
+//  Created by Prathap Reddy on 29/09/23.
+//
+
+import SwiftUI
+
+// MARK: - THEME DATA
+
+let themeData: [Theme] = [
+  Theme(id: 0, themeName: "Pink theme", themeColor: Color.pink),
+  Theme(id: 1, themeName: "Blue theme", themeColor: Color.blue),
+  Theme(id: 2, themeName: "Green theme", themeColor: Color.green)
+]
